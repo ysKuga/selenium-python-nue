41369fa16d150cdf27f4932dd311d14120940bf5
yoshihiko <horrormovies.noga@gmail.com>
2018-11-13 00:47:24 +0900

    feat: element 使用によりにょろ

--- app/test.py
+++ app/test.py
@@ -2,10 +2,14 @@ import sys
 import os
 import time
 from modules.driver import factory as driver_factory
+from modules.element import factory as element_factory
 
 driver = driver_factory()
 
 driver.get('https://google.co.jp/')
+
+element = element_factory(driver)
+element.find('//input[@id="lst-ib"]').send_keys('にょろ')
 
 time.sleep(5)
 
