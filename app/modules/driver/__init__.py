e48f16c837b9d0e1b24cab7ed39ee5d2758d46ab
yoshihiko <horrormovies.noga@gmail.com>
2019-06-11 06:03:55 +0900

    fix: None を除外

--- app/modules/driver/__init__.py
+++ app/modules/driver/__init__.py
@@ -10,9 +10,10 @@ class DriverTypes(Enum):
 
 
 def factory(driver_type=None) -> webdriver:
-    if driver_type is DriverTypes.firefox or re.search('firefox', driver_type, re.IGNORECASE):
-        return firefox.factory()
-    elif driver_type is DriverTypes.chrome or re.search('chrome', driver_type, re.IGNORECASE):
-        return chrome.factory()
+    if driver_type is not None:
+        if driver_type is DriverTypes.firefox or re.search('firefox', driver_type, re.IGNORECASE):
+            return firefox.factory()
+        elif driver_type is DriverTypes.chrome or re.search('chrome', driver_type, re.IGNORECASE):
+            return chrome.factory()
 
     return chrome.factory()
